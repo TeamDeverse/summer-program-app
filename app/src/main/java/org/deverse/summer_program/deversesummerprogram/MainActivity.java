6ef15c3ca972e09a21c03e7be30a93bddaa7f83b
Kevin Mannix <kmannix@bu.edu>
2015-07-16 19:21:34 -0400

    Starting on login view with team

--- app/src/main/java/org/deverse/summer_program/deversesummerprogram/MainActivity.java
+++ app/src/main/java/org/deverse/summer_program/deversesummerprogram/MainActivity.java
@@ -2,46 +2,49 @@ package org.deverse.summer_program.deversesummerprogram;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
-import org.deverse.summer_program.deversesummerprogram.ParseAPI;
+import android.widget.TextView;
+import com.parse.ParseException;
+import com.parse.SignUpCallback;
+
+//import org.deverse.summer_program.deversesummerprogram.ParseAPI;
 
 
 public class MainActivity extends Activity {
 
     // Declare our variables up here so that they're globally accessible. We'll define them later
-    ImageView falloutImage;
-    Button mainButton;
-    ParseAPI parse;
+    ParseAPI backend;
+    EditText loginEmail;
+    EditText loginPassword;
+    Button loginButton;
 
     // The onCreate method is the first method that runs when the app starts up. It's like Java's
     // main() method
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        parse = new ParseAPI(this);
+        backend = new ParseAPI(this);
+        loginEmail = (EditText) findViewById(R.id.login_email);
+        loginPassword = (EditText) findViewById(R.id.login_password);
+        loginButton = (Button) findViewById(R.id.login_button);
 
-        // Define the variables here. Using the `findViewById` method, Android searches the
-        // view we've set for this Activity and then returns a reference to the view with
-        // the name we've input - for the two below, we use "fallout" and "main_button" as names
-        // since that's what we've named our variables in the activity_main.xml file
-        falloutImage = (ImageView) findViewById(R.id.fallout);
-        mainButton = (Button) findViewById(R.id.main_button);
 
-        // Here, we attach an instance of a clickListener onto the main button. This just waits
-        // for a click, and when one occurs, executes the onClick() method and passes in the view
-        // that was clicked itself as an object. We've already defined `falloutImage`, and since it
-        // was originally invisible we make it visible now
-        mainButton.setOnClickListener(new View.OnClickListener() {
+        View.OnClickListener loginClick = new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                   falloutImage.setVisibility(View.VISIBLE);
+                String email = loginEmail.getText().toString();
+                String password = loginPassword.getText().toString();
             }
-        });
+        };
+
+        loginButton.setOnClickListener(loginClick);
     }
 
 }
