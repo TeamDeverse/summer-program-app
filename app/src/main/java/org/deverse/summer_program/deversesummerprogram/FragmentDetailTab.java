31efca9271b1a1fdd9a5d98fca0ea2413c421582
Alex Luh <aluh@mit.edu>
2015-07-28 16:56:28 -0400

    Add map methods to fragment for detail tab

--- app/src/main/java/org/deverse/summer_program/deversesummerprogram/FragmentDetailTab.java
+++ app/src/main/java/org/deverse/summer_program/deversesummerprogram/FragmentDetailTab.java
@@ -1,6 +1,9 @@
 package org.deverse.summer_program.deversesummerprogram;
 
+import android.app.Activity;
 import android.app.Fragment;
+import android.app.FragmentManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,6 +15,9 @@ import android.widget.LinearLayout;
  * Created by aluh on 7/22/2015.
  */
 public class FragmentDetailTab extends Fragment {
+
+    private static GoogleMap mMap; // Might be null if Google Play services APK is not available.
+    final Activity thisActivity = this.getActivity();
 
     Button locationButton;
     LinearLayout locationLayout;
@@ -64,6 +70,87 @@ public class FragmentDetailTab extends Fragment {
         locationButton.setOnClickListener(locationClick);
         contactButton.setOnClickListener(contactClick);
 
+        // Set up the map
+        setUpMapIfNeeded();
+
         return rootView;
+    }
+
+    private MapFragment getMapFragment() {
+        FragmentManager fm = null;
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            fm = getFragmentManager();
+        } else {
+            fm = getChildFragmentManager();
+        }
+
+        return (MapFragment) fm.findFragmentById(R.id.map);
+    }
+
+    /***** Sets up the map if it is possible to do so *****/
+    public void setUpMapIfNeeded() {
+
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            //mMap = ((MapFragment) self.getFragmentManager().findFragmentById(R.id.map)).getMap();
+
+            mMap = getMapFragment().getMap();
+            // Check if we were successful in obtaining the map.
+            if (mMap != null) {
+                setUpMap();
+            }
+        }
+    }
+
+    /**
+     * This is where we can add markers or lines, add listeners or move the
+     * camera.
+     * <p>
+     * This should only be called once and when we are sure that {@link #mMap}
+     * is not null.
+     */
+    private static void setUpMap() {
+        // For showing a move to my loction button
+        mMap.setMyLocationEnabled(true);
+        // For dropping a marker at a point on the Map
+        mMap.addMarker(new MarkerOptions().position(new LatLng(latitude, longitude)).title("My Home").snippet("Home Address"));
+        // For zooming automatically to the Dropped PIN Location
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(latitude,
+                longitude), 12.0f));
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        if (mMap != null)
+            setUpMap();
+
+        if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            //mMap = ((MapFragment) self.getFragmentManager().findFragmentById(R.id.map)).getMap();// getMap is deprecated
+            mMap = getMapFragment().getMap();
+            // Check if we were successful in obtaining the map.
+            if (mMap != null) {
+                setUpMap();
+            }
+        }
+    }
+
+    /**** The MapFragment's id must be removed from the FragmentManager
+     **** or else if the same it is passed on the next time then
+     **** app will crash ****/
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        if (mMap != null) {
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+                getFragmentManager().beginTransaction().remove(getFragmentManager().findFragmentById(R.id.map)).commit();
+            } else {
+                getChildFragmentManager().beginTransaction().remove(getChildFragmentManager().findFragmentById(R.id.map)).commit();
+            }
+
+            mMap = null;
+        }
     }
 }
