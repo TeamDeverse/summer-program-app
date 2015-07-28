89cfb964c8ef9857024efe92fd274fc9d9f77d50
Alex Luh <aluh@mit.edu>
2015-07-28 16:44:53 -0400

    Add FragmentManager and extend FragmentActivity

--- app/src/main/java/org/deverse/summer_program/deversesummerprogram/SiteProfileActivity.java
+++ app/src/main/java/org/deverse/summer_program/deversesummerprogram/SiteProfileActivity.java
@@ -3,20 +3,29 @@ package org.deverse.summer_program.deversesummerprogram;
 import android.app.ActionBar;
 import android.app.Activity;
 import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
 
-public class SiteProfileActivity extends Activity {
+public class SiteProfileActivity extends FragmentActivity {
 
     // Declare tab variables
     ActionBar.Tab Detail, Schedule;
     Fragment fragmentDetailTab = new FragmentDetailTab();
     Fragment fragmentScheduleTab = new FragmentScheduleTab();
+    //String siteId;
+    public static FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_site_profile);
+
+        // Temporary, used for testing
+        //siteId = "8BsaxGFW1Q";
+
+        fragmentManager = getFragmentManager();
 
         ActionBar actionBar = getActionBar();
 
