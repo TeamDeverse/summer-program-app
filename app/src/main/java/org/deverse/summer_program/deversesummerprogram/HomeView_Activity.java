e463b86d19abcda6406085b98045633f9069e9d5
Alex Luh <aluh@mit.edu>
2015-07-29 21:17:57 -0400

    Implemented click listeners for all buttons
    aluh tzwick

--- app/src/main/java/org/deverse/summer_program/deversesummerprogram/HomeView_Activity.java
+++ app/src/main/java/org/deverse/summer_program/deversesummerprogram/HomeView_Activity.java
@@ -10,6 +10,7 @@ import android.text.style.UnderlineSpan;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 
@@ -18,6 +19,10 @@ public class HomeView_Activity extends Activity {
     // Declare our variables up here so that they're globally accessible.
     ParseAPI backend;
     TextView logout;
+    Button accountButton;
+    Button appointmentsButton;
+    Button searchButton;
+    Button sitesButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +31,10 @@ public class HomeView_Activity extends Activity {
 
         // Get all the views
         logout = (TextView) findViewById(R.id.log_out);
+        accountButton = (Button) findViewById(R.id.Account_button);
+        appointmentsButton = (Button) findViewById(R.id.Appointment_button);
+        searchButton = (Button) findViewById(R.id.Search_button);
+        sitesButton = (Button) findViewById(R.id.Sites_button);
 
         String logout_Text = new String("Logout");
         SpannableString content = new SpannableString(logout_Text);
@@ -34,6 +43,16 @@ public class HomeView_Activity extends Activity {
 
         // Create intents for going to other pages
         final Intent goToMainActivity = new Intent(this, MainActivity.class);
+        // Two of the buttons (Account and Appointments) take the user to different tabs
+        //     in the ProfileActivity. Do we need two separate intents that go straight to the
+        //     correct tab? Perhaps code in ProfileActivity.java can distinguish the intents
+        //     and choose which tap is displayed
+        final Intent goToProfileView = new Intent(this, ProfileActivity.class);
+        final Intent goToAppointmentsView = new Intent(this, ProfileActivity.class);
+        // Same deal with two buttons leading to different tabs in SearchFiltering
+        final Intent goToAvailabilitySearch = new Intent(this, SearchFilteringActivity.class);
+        final Intent goToLocationSearch = new Intent(this, SearchFilteringActivity.class);
+
 
         // OnClickListeners for each clickable text or button
         View.OnClickListener logoutClick = new View.OnClickListener() {
@@ -44,7 +63,39 @@ public class HomeView_Activity extends Activity {
             }
         };
 
+        View.OnClickListener accountClick = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(goToProfileView);
+            }
+        };
+
+        View.OnClickListener appointmentsClick = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(goToAppointmentsView);
+            }
+        };
+
+        View.OnClickListener searchClick = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(goToAvailabilitySearch);
+            }
+        };
+
+        View.OnClickListener sitesClick = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(goToLocationSearch);
+            }
+        };
+
         // Set the OnClickListeners
         logout.setOnClickListener(logoutClick);
+        accountButton.setOnClickListener(accountClick);
+        appointmentsButton.setOnClickListener(appointmentsClick);
+        searchButton.setOnClickListener(searchClick);
+        sitesButton.setOnClickListener(sitesClick);
     }
 }
