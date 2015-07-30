1c7d73bca28e75235894558a8885100d170e8756
Alex Luh <aluh@mit.edu>
2015-07-29 20:55:51 -0400

    Implement logout
    aluh tzwick
    Create an OnClickListener for the clickable logout text.
    It will logout the user and return to the main page.

--- app/src/main/java/org/deverse/summer_program/deversesummerprogram/HomeView_Activity.java
+++ app/src/main/java/org/deverse/summer_program/deversesummerprogram/HomeView_Activity.java
@@ -1,6 +1,7 @@
 package org.deverse.summer_program.deversesummerprogram;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Paint;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -8,11 +9,14 @@ import android.text.SpannableString;
 import android.text.style.UnderlineSpan;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 
 public class HomeView_Activity extends Activity {
 
+    // Declare our variables up here so that they're globally accessible.
+    ParseAPI backend;
     TextView logout;
 
     @Override
@@ -20,11 +24,27 @@ public class HomeView_Activity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_view_);
 
+        // Get all the views
         logout = (TextView) findViewById(R.id.log_out);
 
         String logout_Text = new String("Logout");
         SpannableString content = new SpannableString(logout_Text);
         content.setSpan(new UnderlineSpan(), 0, logout_Text.length(), 0);
         logout.setText(content);
+
+        // Create intents for going to other pages
+        final Intent goToMainActivity = new Intent(this, MainActivity.class);
+
+        // OnClickListeners for each clickable text or button
+        View.OnClickListener logoutClick = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                backend.logoutCurrentUser();
+                startActivity(goToMainActivity);
+            }
+        };
+
+        // Set the OnClickListeners
+        logout.setOnClickListener(logoutClick);
     }
 }
--- app/src/main/java/org/deverse/summer_program/deversesummerprogram/ParseAPI.java
+++ app/src/main/java/org/deverse/summer_program/deversesummerprogram/ParseAPI.java
@@ -4,17 +4,23 @@ import android.content.Context;
 
 import com.parse.CountCallback;
 import com.parse.FindCallback;
+import com.parse.FunctionCallback;
 import com.parse.GetCallback;
 import com.parse.LogInCallback;
 import com.parse.Parse;
+import com.parse.ParseCloud;
 import com.parse.ParseException;
+import com.parse.ParseGeoPoint;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 import com.parse.SignUpCallback;
 
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 class ParseAPI {
 
@@ -24,24 +30,149 @@ class ParseAPI {
         this.context = context;
     }
 
-    // Pass in a method to execute when the query runs
-    public void getAllSites(FindCallback<ParseObject> callback) {
+
+
+    /* * * * * * * *
+     *
+     * SITE METHODS
+     *
+     * * * * * * * */
+
+    // Gets all the sites
+
+    public void getAllSites(FindCallback callback) {
         ParseQuery<ParseObject> query = new ParseQuery<ParseObject>("Sites");
         query.findInBackground(callback);
     }
 
-    public void getAvailableTimesForDate(ParseObject site, Date day, FindCallback<ParseObject> callback) {
+
+
+    // Get sites by a location. Pass in a latitude and longitude, and it will order sites by
+    // proximity to that latitude/longitude combination
+    public void getSitesByLocation(double latitude, double longitude, FindCallback callback) {
+        ParseQuery<ParseObject> query = new ParseQuery<ParseObject>("Sites");
+        ParseGeoPoint currentLocation = new ParseGeoPoint(latitude, longitude);
+        query.whereNear("geolocation", currentLocation);
+        query.findInBackground(callback);
+    }
+
+    /* * * * * * * *
+     *
+     * TIME METHODS
+     *
+     * * * * * * * */
+
+    // Gets all the upcoming times for every site
+
+    public void getTimes(FindCallback callback) {
+        ParseQuery<ParseObject> query = new ParseQuery<ParseObject>("Times");
+        query.orderByDescending("date");
+        query.findInBackground(callback);
+    }
+
+    // Pass in a Java Date function, will pass back all the upcoming times for all sites for a date
+
+    public void getTimesByDate(Date date, FindCallback callback) {
+        ParseQuery<ParseObject> query = new ParseQuery<ParseObject>("Times");
+        query.whereEqualTo("date", date);
+        query.orderByDescending("start_hour");
+        query.findInBackground(callback);
+    }
+
+    // Pass in a Site object, will pass back all the times
+
+    public void getTimesForSite(String site_id, FindCallback callback) {
+        ParseQuery<ParseObject> query = new ParseQuery<ParseObject>("Times");
+        query.whereEqualTo("site_id", site_id);
+        query.orderByDescending("date");
+        query.findInBackground(callback);
+    }
+
+    // Pass in a Java Date function for a day with a Site object, and will pass back all the times for that day
+
+    public void getTimesForSiteByDate(String site_id, Date day, FindCallback callback) {
         ParseQuery<ParseObject> query = new ParseQuery<ParseObject>("Times");
         query.whereEqualTo("date", day);
-        query.whereEqualTo("Site", site);
+        query.whereEqualTo("site_id", site_id);
+        query.orderByDescending("start_hour");
         query.findInBackground(callback);
     }
 
-    public void getTimes(FindCallback<ParseObject> callback) {
+    // Pass in a Java Date function for a day with a Site object, will pass back all the upcoming times
+
+    public void getTimesForSiteAfterDate(String site_id, Date day, FindCallback callback) {
         ParseQuery<ParseObject> query = new ParseQuery<ParseObject>("Times");
+        query.whereEqualTo("site_id", site_id);
+        query.whereGreaterThan("date", day);
+        query.orderByDescending("date");
         query.findInBackground(callback);
     }
 
+    /* * * * * * * *
+     *
+     * SIGNUP METHODS
+     *
+     * * * * * * * */
+
+    // Pass in a site object, and will return all the Time objects a user is signed up for
+
+    public void getTimesForSiteWithUserSignups(String site_id, FunctionCallback<Map<String, List<ParseObject>>> callback) {
+        System.out.println("IN HERE fff");
+        ParseQuery<ParseObject> query = new ParseQuery<ParseObject>("Signups");
+        Map<String, String> values = new HashMap<String, String>();
+        values.put("site_id", site_id);
+//        values.put("user_id", this.getCurrentUser().getObjectId());
+        values.put("user_id", "UrIuR0kj4b");
+        ParseCloud.callFunctionInBackground("getTimesForSiteWithUserSignups", values, callback);
+    }
+
+    // Pass in a site object, and will return all the Time objects a user is signed up for
+    public void getTimesForSiteWithUserSignupsByDate(String site_id, Date day, FunctionCallback<Map<String, List<ParseObject>>> callback) {
+        ParseQuery<ParseObject> query = new ParseQuery<ParseObject>("Signups");
+        Map<String, String> values = new HashMap<String, String>();
+        values.put("site_id", site_id);
+        values.put("user_id", this.getCurrentUser().getObjectId());
+        values.put("date", day.toString());
+        ParseCloud.callFunctionInBackground("getTimesForSiteWithUserSignupsByDate", values, callback);
+    }
+
+    // Get all user signups
+    public void getUserSignups(FindCallback callback) {
+        ParseQuery<ParseObject> query = new ParseQuery<ParseObject>("Signups");
+        query.whereEqualTo("user_id", this.getCurrentUser().getObjectId());
+        query.findInBackground(callback);
+    }
+
+    // SIGNING UP FOR A VOLUNTEER TIME IS BELOW
+
+    // Pass in a Time object to sign a user up. No callback needed
+    public void signupForTime(ParseObject time) {
+        ParseObject signup = new ParseObject("Signups");
+
+        signup.put("user_id", this.getCurrentUser().getObjectId());
+        signup.put("time_id", time.getObjectId());
+        signup.put("site_id", time.get("site_id"));
+        signup.saveInBackground();
+    }
+
+    // Pass in a Time object to sign a user up. There is a callback as well which will run after the
+    // signup is saved
+    public void signupForTimeWithCallback(ParseObject time, SaveCallback callback) {
+        ParseObject signup = new ParseObject("Signups");
+
+        signup.put("user_id", this.getCurrentUser().getObjectId());
+        signup.put("time_id", time.getObjectId());
+        signup.put("site_id", time.get("site_id"));
+        signup.saveInBackground(callback);
+    }
+
+    /* * * * * * * *
+     *
+     * USER METHODS
+     *
+     * * * * * * * */
+
+    // Pass in an email, password, first name and last name, and a user will be created
     public void signUpUser(String email, String password, String firstName, String lastName, SignUpCallback callback) {
         ParseUser user = new ParseUser();
         user.setUsername(email);
@@ -49,16 +180,21 @@ class ParseAPI {
         user.put("first_name", firstName);
         user.put("last_name", lastName);
         user.signUpInBackground(callback);
+        user.put("first-name",firstName);
+        user.put("last_name",lastName);
     }
 
+    // Logs in a user with an email and a password, will return the user
     public void loginUser(String email, String password, LogInCallback callback) {
         ParseUser.logInInBackground(email, password, callback);
     }
 
+    // If a user has logged in, this will return the current user
     public ParseUser getCurrentUser() {
         return ParseUser.getCurrentUser();
     }
 
+    // Will log a user out
     public void logoutCurrentUser() {
         ParseUser.logOut();
     }
