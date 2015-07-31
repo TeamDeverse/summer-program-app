060e753f456eb4e14ec6078d9a053816fe00842b
Taibo Li <litb91@gmail.com>
2015-07-31 02:20:57 -0400

    map search working

--- app/src/main/java/org/deverse/summer_program/deversesummerprogram/TabOneFragment.java
+++ app/src/main/java/org/deverse/summer_program/deversesummerprogram/TabOneFragment.java
@@ -15,6 +15,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ActionMenuView;
 import android.widget.Button;
+import android.widget.CalendarView;
 import android.widget.DatePicker;
 import android.widget.LinearLayout;
 import android.widget.TimePicker;
@@ -25,12 +26,20 @@ public class TabOneFragment extends Fragment {
 
     final Activity self = this.getActivity();
 
+    CalendarView searchCalendar;
+    TimePicker timePicker;
+
     Button dateSelectionButton;
     Button timeSelectionButton;
     Button searchButton;
+    Button saveTimeButton;
 
     LinearLayout calendarLinearLayout;
     LinearLayout timepickerLinearLayout;
+
+    String selectedDay, selectedTime;
+
+    private String format = "";
 
 
 
@@ -42,9 +51,13 @@ public class TabOneFragment extends Fragment {
         dateSelectionButton=(Button)rootView.findViewById(R.id.buttonSelectDate);
         timeSelectionButton=(Button)rootView.findViewById(R.id.buttonSelectTime);
         searchButton=(Button)rootView.findViewById(R.id.buttonSearchDateTime);
+        saveTimeButton = (Button) rootView.findViewById(R.id.time_save_button);
 
         calendarLinearLayout = (LinearLayout) rootView.findViewById(R.id.calendar_linearLayout);
         timepickerLinearLayout = (LinearLayout) rootView.findViewById(R.id.timepicker_linearLayout);
+
+        searchCalendar = (CalendarView) rootView.findViewById(R.id.calendar_search);
+        timePicker = (TimePicker) rootView.findViewById(R.id.timepicker_search);
 
         // Set ClickListener on btnSelectDate
         dateSelectionButton.setOnClickListener(new View.OnClickListener() {
@@ -66,16 +79,41 @@ public class TabOneFragment extends Fragment {
         timeSelectionButton.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View v) {
-                if (calendarLinearLayout.getVisibility()==View.VISIBLE){
+                if (calendarLinearLayout.getVisibility() == View.VISIBLE) {
                     calendarLinearLayout.setVisibility(View.GONE);
                 }
-                if (timepickerLinearLayout.getVisibility() == View.VISIBLE){
+                if (timepickerLinearLayout.getVisibility() == View.VISIBLE) {
                     timepickerLinearLayout.setVisibility(View.GONE);
                 } else {
                     timepickerLinearLayout.setVisibility(View.VISIBLE);
                 }
             }
         });
+
+        // Set ClickListener on btnSelectTime
+        saveTimeButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                selectedTime = timePicker.getCurrentHour() + ":" + timePicker.getCurrentMinute();
+                timeSelectionButton.setText(selectedTime);
+            }
+        });
+
+        searchCalendar.setShowWeekNumber(false);
+
+        CalendarView.OnDateChangeListener dateChangeListener = new CalendarView.OnDateChangeListener(){
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int day){
+                month++;
+                selectedDay = month + "/" + day + "/" + year;
+                dateSelectionButton.setText(selectedDay);
+            }
+        };
+
+        searchCalendar.setOnDateChangeListener(dateChangeListener);
+
+
+
 
 
 
--- app/src/main/java/org/deverse/summer_program/deversesummerprogram/TabTwoFragment.java
+++ app/src/main/java/org/deverse/summer_program/deversesummerprogram/TabTwoFragment.java
@@ -3,18 +3,30 @@ package org.deverse.summer_program.deversesummerprogram;
 
 import android.app.Activity;
 import android.app.FragmentManager;
+import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Build;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import android.net.Uri;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.List;
 
 
 /**
@@ -23,8 +35,18 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class TabTwoFragment extends Fragment {
 
     private static GoogleMap mMap; // Might be null if Google Play services APK is not available.
+
     private static Double latitude, longitude;
 
+    private double mlatitude, mlongitude; // to store user picked coordinates
+    private String coordinateText;
+
+    EditText addrText;
+    Button mapSearchButton;
+
+    TextView locationData;
+
+    private final String TAG = "MapLocation";
     final Activity self = this.getActivity();
 
 
@@ -36,13 +58,79 @@ public class TabTwoFragment extends Fragment {
         }
         View rootView = inflater.inflate(R.layout.fragment_tab_two, container, false);
 
-        // Washington DC
-        latitude = 38.889;
-        longitude = -77.0352;
+        addrText = (EditText) rootView.findViewById(R.id.enter_location);
+        mapSearchButton = (Button) rootView.findViewById(R.id.mapButton);
+
+        locationData = (TextView) rootView.findViewById(R.id.coordinates);
+
+        mapSearchButton.setOnClickListener(new View.OnClickListener() {
+
+            // Called when user clicks the Show Map button
+            public void onClick(View v) {
+                try {
+
+                    // Process text for network transmission
+                    String address = addrText.getText().toString();
+                    address = address.replace(' ', '+');
+
+                    // Create Intent object for starting Google Maps application
+//                    Intent geoIntent = new Intent(
+//                            android.content.Intent.ACTION_VIEW, Uri
+//                            .parse("geo:0,0?q=" + address));
+
+//                    // Use the Intent to start Google Maps application using Activity.startActivity()
+//                    startActivity(geoIntent);
+
+                    LatLng latlng = getLocationFromAddress(address);
+
+                    latitude = latlng.latitude;
+                    longitude = latlng.longitude;
+
+                    refreshMap();
+
+
+                } catch (Exception e) {
+                    // Log any error messages to LogCat using Log.e()
+                    Log.e(TAG, e.toString());
+                }
+            }
+        });
+
+        // Buzzlab
+        latitude = 42.350529;
+        longitude = -71.099855;
 
         setUpMapIfNeeded();
 
+        // Washington DC
+
+
         return rootView;
+    }
+
+    public LatLng getLocationFromAddress(String strAddress) {
+
+        Geocoder coder = new Geocoder(getActivity());
+        List<Address> address;
+        LatLng p1 = null;
+
+        try {
+            address = coder.getFromLocationName(strAddress, 5);
+            if (address == null) {
+                return null;
+            }
+            Address location = address.get(0);
+            location.getLatitude();
+            location.getLongitude();
+
+            p1 = new LatLng(location.getLatitude(), location.getLongitude() );
+
+        } catch (Exception ex) {
+
+            ex.printStackTrace();
+        }
+
+        return p1;
     }
 
     private MapFragment getMapFragment() {
@@ -75,19 +163,52 @@ public class TabTwoFragment extends Fragment {
     /**
      * This is where we can add markers or lines, add listeners or move the
      * camera.
-     * <p>
      * This should only be called once and when we are sure that {@link #mMap}
      * is not null.
      */
     private static void setUpMap() {
+
+        mMap.clear();
         // For showing a move to my loction button
         mMap.setMyLocationEnabled(true);
         // For dropping a marker at a point on the Map
-        mMap.addMarker(new MarkerOptions().position(new LatLng(latitude, longitude)).title("My Home").snippet("Home Address"));
-        // For zooming automatically to the Dropped PIN Location
-        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(latitude,
-                longitude), 12.0f));
+        final LatLng posCor = new LatLng(latitude, longitude);
+        mMap.addMarker(new MarkerOptions().position(posCor).title("My Home").snippet("Home Address"));
+
+       // For zooming automatically to the Dropped PIN Location
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(posCor, 12.0f));
     }
+
+
+    private void refreshMap() {
+
+        mMap.clear();
+        // For showing a move to my loction button
+        mMap.setMyLocationEnabled(true);
+        // For dropping a marker at a point on the Map
+        final LatLng posCor = new LatLng(latitude, longitude);
+        final Marker myPosition = mMap.addMarker(new MarkerOptions().position(posCor).title("My Home").snippet("Home Address"));
+        // Set onclick event
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                if(marker.equals(myPosition)){
+                    mlatitude = marker.getPosition().latitude;
+                    mlongitude = marker.getPosition().longitude;
+                    coordinateText = "Your chosen coordinate is " + String.valueOf(mlatitude) + ',' + String.valueOf(mlatitude);
+                    locationData.setText(coordinateText);
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        });
+
+        // For zooming automatically to the Dropped PIN Location
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(posCor, 12.0f));
+    }
+
+
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
