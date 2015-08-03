ef57ef4eb542990d5386ddcddf389e31e581c8f2
Alex Luh <aluh@mit.edu>
2015-08-02 20:32:50 -0400

    Connect pages of the app together
    Navigate from the login/signup main page to welcome to home view to search to
    site profile

--- app/src/main/java/org/deverse/summer_program/deversesummerprogram/WelcomeActivity.java
+++ app/src/main/java/org/deverse/summer_program/deversesummerprogram/WelcomeActivity.java
@@ -6,12 +6,15 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 
 public class WelcomeActivity extends Activity {
 
     TextView helloText;
+    Button homeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,11 +22,25 @@ public class WelcomeActivity extends Activity {
         setContentView(R.layout.activity_welcome);
 
         helloText = (TextView) findViewById(R.id.hello_text);
+        homeButton = (Button) findViewById(R.id.home_button);
+
+        final Intent goToHomeViewActivity = new Intent(this, HomeView_Activity.class);
 
         // Get the username from the intent
         Intent fromMainActivity = getIntent();
         String username = fromMainActivity.getStringExtra("username");
         helloText.setText("Welcome back, " + username);
+
+        // ClickListeners for each button
+        View.OnClickListener homeClick = new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                startActivity(goToHomeViewActivity);
+            }
+        };
+
+        // Assign the click listener for each button
+        homeButton.setOnClickListener(homeClick);
 
     }
 }
